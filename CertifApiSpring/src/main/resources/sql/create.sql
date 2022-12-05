35a3dc0a202770ac878e659b923fbb34894cddd1
Alexandra Cantarano <alexandra.cantarano@gmail.com>
2022-12-05 12:02:45 +0100

    modif SQL

--- CertifApiSpring/src/main/resources/sql/create.sql
+++ CertifApiSpring/src/main/resources/sql/create.sql
@@ -1,64 +1,38 @@
-
-
-
-
-
-
-
-
-
-
-DROP TABLE user_channel;
-DROP TABLE messages;
-DROP TABLE users;
-DROP TABLE roles;
-DROP TABLE canaux;
-
-CREATE TABLE canaux(
-    id INT(20) PRIMARY KEY AUTO_INCREMENT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    description TEXT,
-    name VARCHAR(255) NOT NULL,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP 
-);
-
-CREATE TABLE roles(
-    id INT(20) PRIMARY KEY AUTO_INCREMENT,
-    role VARCHAR(255)
+CREATE TABLE channels(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    channel_name VARCHAR(255) NOT NULL,
+    created_at DATE,
+    updated_at DATE,
+    description TEXT
 );
 
 CREATE TABLE users(
-    id INT(20) PRIMARY KEY AUTO_INCREMENT,
-    email VARCHAR(100) NOT NULL,
-    password VARCHAR(50) NOT NULL,
-    username VARCHAR(100) NOT NULL,
-    role_id INT(20),
-    CONSTRAINT fk_user_role FOREIGN KEY (role_id) REFERENCES roles(id)
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    lastname VARCHAR(255) NOT NULL,
+    firstname VARCHAR(255) NOT NULL,
+    pseudo VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE users_channels(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    channel_id INT NOT NULL,
+    CONSTRAINT fk_user_channel_user FOREIGN KEY (user_id) REFERENCES users(id),
+    CONSTRAINT fk_user_channel FOREIGN KEY (channel_id) REFERENCES channels(id)
 );
 
 CREATE TABLE messages(
-    id INT(20) PRIMARY KEY AUTO_INCREMENT,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     content TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
-    canal_id INT(20),
-    user_id INT(20),
-    CONSTRAINT fk_message_canal FOREIGN KEY (canal_id) REFERENCES canaux(id),
-    CONSTRAINT fk_message_user FOREIGN KEY (user_id) REFERENCES users(id)
+    created_at DATE NOT NULL,
+    updated_at DATE,
+    user_id INT NOT NULL,
+    channel_id INT NOT NULL,
+    CONSTRAINT fk_message_user FOREIGN KEY (user_id) REFERENCES users(id),
+    CONSTRAINT fk_message_channel FOREIGN KEY (channel_id) REFERENCES channels(id)
 );
-
-CREATE TABLE user_channel(
-    id INT(20) PRIMARY KEY AUTO_INCREMENT,
-    canal_id INT(20),
-    user_id INT(20),
-    CONSTRAINT fk_user_canal FOREIGN KEY (canal_id) REFERENCES canaux(id),
-    CONSTRAINT fk_canal_user FOREIGN KEY (user_id) REFERENCES users(id)
-);
-
-
-
-
-
 
 
 
